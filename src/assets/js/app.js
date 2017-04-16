ebc08ad0b7041be150358c145459fa27008d1999
RDNyel <al12542062@unadmexico.mx>
2017-04-16 00:56:23 -0500

    +1:

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,16 @@
 $(document).foundation();
 
 $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
- if(newSize ==='small' && oldSize ==='medium' ) {
-     $('#menu-principal').removeClass('expanded');
- }
- if(newSize ==='medium' && oldSize ==='small' ) {
-     $('#menu-principal').addClass('expanded');
- }
+   if(newSize ==='small' && oldSize ==='medium' ) {
+       $('#menu-principal').removeClass('expanded');
+   }
+   if(newSize ==='medium' && oldSize ==='small' ) {
+       $('#menu-principal').addClass('expanded');
+   }
 
 });
 
 var size = Foundation.MediaQuery.current;
 if(size == "small") {
-  $('#menu-principal').removeClass('expanded');
+    $('#menu-principal').removeClass('expanded');
 }
