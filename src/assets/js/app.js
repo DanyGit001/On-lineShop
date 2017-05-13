a7f609aacebe92228beb5fdbc5fefce130f2d2a9
RDNyel <al12542062@unadmexico.mx>
2017-05-13 06:49:45 -0500

    +1:

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,7 +10,22 @@ $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
 
 });
 
+//toma el tamaño principal al abrir el sitio y agrega clase basado en tamaño
 var size = Foundation.MediaQuery.current;
 if(size == "small") {
     $('#menu-principal').removeClass('expanded');
 }
+
+//agrega clase basado en el scroll que se la haga a la ventana principal
+
+var navegacion = $('#navegacion-principal');
+var nav = new Foundation.Magellan(navegacion);
+
+$(window).scroll(function() {
+    if(nav.scrollPos >= 300) {
+        $(navegacion).addClass('fixed');
+    } else {
+$(navegacion).removeClass('fixed');
+    
+    }
+});
